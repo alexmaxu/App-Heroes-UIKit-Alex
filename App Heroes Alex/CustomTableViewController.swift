2d35692eefb1e425d2cac34ebf31ec28560af7d5
alex <alexmaxu12@gmail.com>
2024-03-24 23:06:36 +0100

    borrado de numberOfSections

--- App Heroes Alex/CustomTableViewController.swift
+++ App Heroes Alex/CustomTableViewController.swift
@@ -23,10 +23,7 @@ class CustomTableViewController: UITableViewController {
 
     // MARK: - Table view data source
 
-    override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 1
-    }
+  
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
